37d851fa41ed7703868fb9734ea8f6d2066fa40f
ps-io <piyushsayal3@gmail.com>
2021-12-28 23:48:36 +1100

    up files

---
+++ config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+hwclock --systohc
+
+echo "en_AU.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+echo "LANG=en_AU.UTF-8" >> /etc/locale.conf
+
+echo "ArchLite" >> /etc/hostname
+
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 ArchLite.localdomain ArchLite" >> /etc/hosts
+
+pacman -S grub efibootmgr dosfstools mtools os-prober networkmanager network-manager-applet wireless_tools
+
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
+echo "---Create Root Password & New User---"
+
---
+++ post.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+git clone https://aur.archlinux.org/yay.git
+cd yay;makepkg -si -noconfirm;cd;rm -r yay
+
+sudo pacman -S --noconfirm xorg ttf-dejavu git polkit-gnome lightdm lightdm-gtk-greeter
+
+sudo pacman -S arandr bash-completion bluez bluez-utils cups discord dunst ffmpegthumbnailer firefox firewalld gcolor3 gedit gzip hplip jupyter-notebook jupyterlab linux-headers lxappearance maim moreutils nautilus neofetch nfs-utils nitrogen nodejs npm ntfs-3g pamixer pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-media-session pipewire-pulse polkit-gnome pulsemixer python-pip qbittorrent redshift rofi sqlitebrowser sxiv texlive-most thunar vlc xclip zathura zathura-pdf-mupdf zsh
+
+cd
+repos=( "dmenu" "st" "slock" )
+for repo in ${repos[@]}
+do
+    git clone git://git.suckless.org/$repo
+    cd $repo;make;sudo make install;cd ..
+done
+
+
+https://github.com/yuuushio/dwm.git
+cd $repo;make;sudo make install;cd ..
+
+https://github.com/yuuushio/dwmblocks.git
+cd $repo;make;sudo make install;cd ..
+
+# XSessions and dwm.desktop
+if [[ ! -d /usr/share/xsessions ]]; then
+    sudo mkdir /usr/share/xsessions
+fi
+
+cat > ./temp << "EOF"
+[Desktop Entry]
+Encoding=UTF-8
+Name=Dwm
+Comment=Dynamic window manager
+Exec=~/.dwm/autostart.sj
+Icon=dwm
+Type=XSession
+EOF
+sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
+
+systemctl enable lightdm
+systemctl enable bluetooth
+
+yay -S downgrade otf-symbola nerd-fonts-complete typora-free lf-git timeshift jre ttf-ms-fonts virtualbox-ext-oracle visual-studio-code-bin ttf-mac-fonts libxft-bgra neovim-nightly-bin picom-jonaburg-git jdk powerline-fonts-git
+
+echo "---Configure LightDM; Restart PC---"
+
