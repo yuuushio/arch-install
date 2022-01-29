a28e63fe934181a4c66dba3075d868b1bac05e08
ps-io <piyushsayal3@gmail.com>
2022-01-30 05:58:01 +1100

    add git to pkg

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ echo "127.0.0.1	localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ArchLite.localdomain ArchLite" >> /etc/hosts
 
-pacman -S --no-confirm grub efibootmgr dosfstools mtools os-prober networkmanager network-manager-applet wireless_tools
+pacman -S --no-confirm grub efibootmgr dosfstools mtools os-prober networkmanager network-manager-applet wireless_tools git
 
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
