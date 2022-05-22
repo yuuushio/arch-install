c026b74a01ab25129812d9b42db5582d025e4acf
ps-io <piyushsayal3@gmail.com>
2022-05-23 02:41:39 +1000

    update posh sh

--- post.sh
+++ post.sh
@@ -36,20 +36,26 @@ mv -f typewritten.zsh ~/.oh-my-zsh/custom/themes/typewritten/typewritten.zsh
 mv -f picom.conf ~/.config/picom.conf
 mv -f nvim ~/.config/nvim
 mv nda ~/.themes/nda
+mv .zshrc ~/.zshrc
 sudo mv bluetooth/main.conf /etc/bluetooth/main.conf
 sudo mv pipewire.conf /usr/share/pipewire/pipewire.conf
 # -----------------------
 
-repos=( "st" )
-for repo in ${repos[@]}
-do
-    git clone git://git.suckless.org/$repo
-    cd $repo;make;sudo make install;cd ..
-done
+cd
 
+#repos=( "st" )
+#for repo in ${repos[@]}
+#do
+#    git clone git://git.suckless.org/$repo
+#    cd $repo;make;sudo make install;cd ..
+#done
 
 git clone https://github.com/yuuushio/dwm.git
-cd dwm;make;sudo make install;cd ..
+cd dwm;make;sudo make install;cd
+
+cd dwmblocks;make;sudo make install;cd
+cd st;make;sudo make install;cd
+
 
 # XSessions and dwm.desktop
 if [[ ! -d /usr/share/xsessions ]]; then
@@ -64,7 +70,7 @@ sudo cat > dwm.desktop <<EOF
 Encoding=UTF-8
 Name=Dwm
 Comment=dwm
-Exec=dwm
+Exec=~/.dwm/autostart.sh
 Icon=dwm
 Type=XSession
 EOF
@@ -74,7 +80,7 @@ systemctl enable bluetooth
 
 #sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 #sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-#yay -S downgrade otf-symbola nerd-fonts-complete typora-free lf-git timeshift jre ttf-ms-fonts virtualbox-ext-oracle visual-studio-code-bin ttf-mac-fonts libxft-bgra neovim-nightly-bin picom-jonaburg-git jdk powerline-fonts-git
+#yay -S downgrade otf-symbola nerd-fonts-complete typora-free lf-git timeshift jre ttf-ms-fonts virtualbox-ext-oracle visual-studio-code-bin ttf-mac-fonts libxft-bgra jdk powerline-fonts-git
 
 echo "---Configure LightDM; Restart PC---"
 
