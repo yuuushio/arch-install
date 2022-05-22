6b30849cf3642b4880b249263157c8f3514e1171
ps-io <piyushsayal3@gmail.com>
2022-05-23 02:45:55 +1000

    chroot

--- chroot.sh
+++ chroot.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-read -r -p "Enter drive name (exapmle sda) " drive
+read -r -p "Enter drive name (example sda) " drive
 
-yes | mkfs.vfat /dev/${drive}1
+mkfs.vfat /dev/${drive}1
 
-yes | mkswap /dev/${drive}2
-yes | swapon /dev/${drive}2
+mkswap /dev/${drive}2
+swapon /dev/${drive}2
 
-yes | mkfs.ext4 /dev/${drive}3
+mkfs.ext4 /dev/${drive}3
 
 mount /dev/${drive}3 /mnt
 
 mkdir -p /mnt/efi
 mount /dev/${drive}1 /mnt/efi
 
-pacstrap /mnt base base-devel linux linux-firmware vim nano man sudo amd-ucode
+pacstrap /mnt base base-devel linux linux-firmware vim man sudo amd-ucode
 
 sleep 2
 
 genfstab -U /mnt >> /mnt/etc/fstab
-sleep 3
+sleep 1
 arch-chroot /mnt
