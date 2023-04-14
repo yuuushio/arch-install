ae422dbc6f176c30e57c3779acd7d20e03d7803d
ps-io <piyushsayal3@gmail.com>
2023-04-14 16:46:25 +1000

    fix post.sh

--- post.sh
+++ post.sh
@@ -1,33 +1,57 @@
 #!/bin/bash
 
+
+
 git clone https://aur.archlinux.org/yay.git
 cd yay;makepkg -si;cd;rm -rf yay
+
+sudo pacman -Syu
 
 sudo pacman -S xorg ttf-dejavu git polkit-gnome
 
 sudo pacman -S arandr dmenu bluez bluez-utils cups \
 dunst ffmpegthumbnailer gcolor3 gzip hplip \
 linux-headers lxappearance-gtk3 maim moreutils nfs-utils \
-ntfs-3g pamixer pavucontrol pipewire pipewire-alsa pipewire-jack \
+ntfs-3g pamixer pavucontrol pipewire wireplumber pipewire-alsa pipewire-jack \
 pipewire-pulse pulsemixer python-pip qbittorrent \
-redshift rofi sxiv texlive-most thunar xclip zathura zathura-pdf-mupdf \
-zsh tmux texstudio
+redshift rofi sxiv texlive-most xclip zathura zathura-pdf-mupdf \
+zsh tmux texstudio libmpdclient slock
 
-yay -S neovim-nightly-bin picom-jonaburg-git ly brave-bin
+install_oh_my_zsh() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+}
+
+install_oh_my_zsh
+
+#repos=( "st" "dwm" )
+#for repo in ${repos[@]}
+#do
+#    git clone git://git.suckless.org/$repo
+#    cd $repo;make;sudo make install;cd ..
+#done
 
 
-repos=( "st" "dwm" )
-for repo in ${repos[@]}
-do
-    git clone git://git.suckless.org/$repo
-    cd $repo;make;sudo make install;cd ..
+##########################
+# Clone the Succless repository
+git clone https://github.com/yuuushio/Succless.git
+
+# Go into the Succless directory
+cd Succless
+
+# Declare an array containing the component directories
+components=("dwm" "st" "dwblocks")
+
+# Iterate over the components and build each one
+for component in "${components[@]}"; do
+  echo "Building $component..."
+  cd "$component"
+  make
+  sudo make clean install
+  cd ..
+  echo "$component built and installed."
 done
-
-git clone https://github.com/yuuushio/dwm.git
-cd dwm;make;sudo make install;cd
-
-cd ~/.config/st;make;sudo make clean install;cd
-cd ~/.config/dwmblocks;make;sudo make clean install;cd
+cd
+##########################
 
 
 # XSessions and dwm.desktop
@@ -48,13 +72,22 @@ Icon=dwm
 Type=XSession
 EOF
 
-systemctl enable ly
-systemctl enable bluetooth
+#systemctl enable ly
+systemctl enable --now bluetooth
+systemctl --user enable --now pipewire.socket
+systemctl --user enable --now pipewire.service # Fix the typo "pipeiwre.service" to "pipewire.service"
+systemctl --user enable --now pipewire-pulse.socket
+systemctl --user enable --now pipewire-pulse.service
 systemctl --user --now enable wireplumber
 
-#sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-#sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-#yay -S downgrade otf-symbola nerd-fonts-complete typora-free lf-git timeshift jre ttf-ms-fonts virtualbox-ext-oracle visual-studio-code-bin ttf-mac-fonts libxft-bgra jdk powerline-fonts-git
+# Install nerd fonts
+cd
+git clone https://github.com/ryanoasis/nerd-fonts.git --depth 1; cd nerd-fonts;sudo ./install.sh;cd ..;rm -rf nerd-fonts
+
+
+sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+yay -S opencl-nvidia-470xx nvidia-470xx-dkms nvidia-470xx-utils otf-symbola jre ttf-ms-fonts ttf-mac-fonts libxft-bgra jdk powerline-fonts-git
 
 echo "---Restart PC---"
 
